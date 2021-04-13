6c4d33e8f579734f02027c439a40ab75c6779e75
hansenwuwu <eric248655665@gmail.com>
2021-04-13 11:48:22 +0800

    add cors

--- webrtc-server-only-message/app/main.py
+++ webrtc-server-only-message/app/main.py
@@ -6,17 +6,17 @@ import json
 
 app = FastAPI()
 
-# origins = [
-#    "*"
-# ]
+origins = [
+   "*"
+]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
