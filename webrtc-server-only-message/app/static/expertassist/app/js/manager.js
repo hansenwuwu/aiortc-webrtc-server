6a98397d1f085f7d6501a3e5b36347b372a7e85e
hansenwuwu <eric248655665@gmail.com>
2021-05-31 15:17:46 +0800

    update ip

--- webrtc-server-only-message/app/static/expertassist/app/js/manager.js
+++ webrtc-server-only-message/app/static/expertassist/app/js/manager.js
@@ -15,7 +15,7 @@ const EndCallEnum = Object.freeze({
 
 // 由於object的reference固定不變，故在其他地方引用之後，仍可改變值
 const status = {
-  url: isProduction ? "192.168.4.49" : "ec2-18-181-179-94.ap-northeast-1.compute.amazonaws.com",
+  url: isProduction ? "192.168.4.23" : "ec2-18-181-179-94.ap-northeast-1.compute.amazonaws.com",
   isSSL: true,
   online: {},
   currentHmd: {},
